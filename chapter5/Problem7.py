8858b66480fc94616051b84d537e2cc1c5ed2b84
C S Narendra Vignesh <csnarendravignesh@gmail.com>
2026-07-10 12:52:15 +0530

    chapter5

---
+++ chapter5/Problem7.py
@@ -0,0 +1,2 @@
+#If the names of two friends are the same :what will happen to the program in problem no 6?
+# The values will be later updated.
---
+++ chapter5/Problem8.py
@@ -0,0 +1,2 @@
+#   What if the values of 2 keys are the same?
+#Nothing will happen the values can be the same in a dictionary.
